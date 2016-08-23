df0794a36aad5e5d658cf4210b00e2ca71aadbfe
Yury Ksenevich <yury@spadar.com>
2016-08-23 21:59:12 +0200

    Ignore developer files

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 /root/magento.sh > /var/log/magento-init/magento-stdout.log 2> /var/log/magento-init/magento-stderr.log && /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
